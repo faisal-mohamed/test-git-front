c38f016defc4002d9f8442d99c30d343c0bed47d
faisal-mohamed <mohamedfaisalm@hotmail.com>
2024-08-21 04:54:00 +0530

    fix: build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 const App = () => {
 
@@ -8,8 +8,20 @@ const App = () => {
   }
   return (
     <div>
+      <BrowserRouter>
+
+      <Link to='/'>Home</Link>
+      <Link to='/about-us'>About us</Link>
+      <Link to='/contact'>Contact</Link>
+      
       <h1>App</h1>
       <button onClick={handleClick}>Click me</button>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/about-us" element={<h1>About us</h1>} />
+      </Routes>
+      </BrowserRouter>
     </div>
     
   )
